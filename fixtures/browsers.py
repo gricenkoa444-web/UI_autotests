938f0f91b634ffad3e60d020855b52066dffa898
gricenkoa444-web <gricenkoa444@gmail.com>
2026-04-14 15:56:21 +0300

    create file - playwright and hchanges made with browser's file

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -4,22 +4,14 @@ from _pytest.fixtures import SubRequest
 from playwright.sync_api import Playwright, Page
 
 from pages.autorization.registration_page import RegistrationPage
+from tools.playwright.pages import initialize_playwright_page
 
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(record_video_dir='./videos')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    page = context.new_page()
+    yield from initialize_playwright_page(playwright,
+                                          test_name=request.node.name)
 
-    yield page
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
-    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
 
 
 @pytest.fixture(scope="session")
@@ -39,15 +31,9 @@ def initialize_browser_state(playwright: Playwright):
 
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json", record_video_dir='./videos')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    page = context.new_page()
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        storage_state="browser-state.json"
+    )
 
-    yield page
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
-    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
